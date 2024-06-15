6e4765fb7ba6904a1a704b515e9dc871c4ef4c03
Kirill Chizhov <sstive39@gmail.com>
2024-06-15 13:07:59 +0300

    Initial project setup

---
+++ CodeMerger.Cli/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeMerger.Cli;
+
+internal sealed class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
---
+++ CodeMerger.Core/Merger.cs
@@ -0,0 +1,5 @@
+namespace CodeMerger.Core;
+
+public class Merger
+{
+}
